1e888c5c8a63a539ac0254fa46a376dd94dfdecc
Michael Schwier <mschwier@bwh.harvard.edu>
2018-05-13 12:32:50 -0400

    Using longer running evironment for testing

--- test.py
+++ test.py
@@ -4,18 +4,27 @@ from agent import DQNAgent
 
 EPISODES = 10
 
+
+# create an environment that runs longer than the default
+def getLongRunEnv():
+  gym.envs.register(
+    id='CartPole-v2',
+    entry_point='gym.envs.classic_control:CartPoleEnv',
+    tags={'wrapper_config.TimeLimit.max_episode_steps': 5000},
+    reward_threshold=4750.0,
+  )
+  return  gym.make('CartPole-v2')
+
+
 if __name__ == "__main__":
-  env = gym.make('CartPole-v1')
+  env = getLongRunEnv()
   state_size = env.observation_space.shape[0]
   action_size = env.action_space.n
   agent = DQNAgent(state_size, action_size, init_exploration_rate=0.0)
   agent.load("model.h5")
 
   for test_episode_idx in range(EPISODES):
-    env.reset()
-    # take a random step in the beginning to not always start the same way
-    action = env.action_space.sample()
-    state, _, _, _ = env.step(action)
+    state = env.reset()
     state = np.reshape(state, [1, state_size]).astype(np.float32)
     score = 0
     game_over = False
